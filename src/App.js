3d98f1d30c5877dd714b2663ba221716dacd20bd
Maryna Darashuk <m.doroshuk@bk.ru>
2022-11-13 21:56:21 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+import { toast, ToastList } from 'toastique-toast'
 
 function App() {
+  const [toasts, setToasts] = useState([])
+  const [properties, setProperties] = useState({});
+
+  const handleShowToast = () => {
+    setToasts(toast.getToasts('info', {
+      heading: "Wow",
+      content: "I'm an info toast!"
+    }))
+
+    setProperties({
+      position: "top-right",
+      autoCloseTime: 3000
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button type='button' onClick={handleShowToast}>Show</button>
+      {toasts.length > 0 && <ToastList toast={toast} toastList={toasts} properties={properties}/>}
     </div>
   );
 }
